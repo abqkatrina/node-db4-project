a4dac268abd742cac6e7bcb2b025b2d51e816426
Katrina Hernandez <abqkatrina@gmail.com>
2020-04-16 14:38:12 -0600

    working server

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
-// Update with your config settings.
-
 module.exports = {
 
     development: {
       client: 'sqlite3',
-      useNullAsDefault: true, // needed for sqlite
+      useNullAsDefault: true,
       connection: {
         filename: './data/schemes.db3',
       },
@@ -14,10 +12,8 @@ module.exports = {
       seeds: {
         directory: './data/seeds'
       },
-      // add the following
       pool: {
         afterCreate: (conn, done) => {
-          // runs after a connection is made to the sqlite engine
           conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
         },
       },
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const recipeRouter = require('./recipes/recipesRouter.js');
+const recipesRouter = require('./recipes/recipesRouter.js');
 
 const server = express();
 
 server.use(express.json());
-server.use('/recipes', recipesRouter);
+server.use('api/recipes', recipesRouter);
 
 module.exports = server;
